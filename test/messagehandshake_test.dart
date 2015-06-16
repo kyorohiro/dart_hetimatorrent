6ca771cbb9c68277e55ec15f4fd70ee707d62524
kyorohiro <kyorohiro@gmail.com>
2015-06-16 23:39:16 +0900

    ..

--- test/messagehandshake_test.dart
+++ test/messagehandshake_test.dart
@@ -7,14 +7,15 @@ import 'dart:typed_data' as type;
 import 'dart:convert' as convert;
 
 void main() {
+  ArrayBuilder builder = new ArrayBuilder();
+  builder.appendByte(19);
+  builder.appendIntList(MessageHandshake.ProtocolId, 0, MessageHandshake.ProtocolId.length);
+  builder.appendIntList([0,0,0,0,0,0,0,0], 0, 8);
+  builder.appendIntList(convert.UTF8.encode("123456789A123456789B"), 0, 20);
+  builder.appendIntList(convert.UTF8.encode("123456789C123456789D"), 0, 20);
+
   unit.group('A group of tests', () {
     unit.test("decode/encode", () {
-      ArrayBuilder builder = new ArrayBuilder();
-      builder.appendByte(19);
-      builder.appendIntList(MessageHandshake.ProtocolId, 0, MessageHandshake.ProtocolId.length);
-      builder.appendIntList([0,0,0,0,0,0,0,0], 0, 8);
-      builder.appendIntList(convert.UTF8.encode("123456789A123456789B"), 0, 20);
-      builder.appendIntList(convert.UTF8.encode("123456789C123456789D"), 0, 20);
       EasyParser parser = new EasyParser(builder);
       return MessageHandshake.decode(parser).then((MessageHandshake message) {
         unit.expect(message.protocolId, MessageHandshake.ProtocolId);//message.
@@ -26,6 +27,27 @@ void main() {
         unit.expect(builder.toList(), data);
       });
     });
+    
+    unit.test("encode", () {
+      EasyParser parser = new EasyParser(builder);
+      MessageHandshake message = new MessageHandshake(MessageHandshake.ProtocolId, [0,0,0,0,0,0,0,0],
+          convert.UTF8.encode("123456789A123456789B"), convert.UTF8.encode("123456789C123456789D"));
 
+      message.encode().then((List<int> data) {
+        unit.expect(builder.toList(), data);
+      });
+    });
+    
+    unit.test("error", () {
+      ArrayBuilder b = new ArrayBuilder.fromList(builder.toList().sublist(0,builder.size()-1));
+      b.fin();
+      EasyParser parser = new EasyParser(b);
+
+      MessageHandshake.decode(parser).then((_) {
+        unit.expect(true,false);
+      }).catchError((e){
+        unit.expect(true,true);
+      });
+    });
   });
 }
