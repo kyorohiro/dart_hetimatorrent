f0edd82bbddafb1b8ee9f18ff18fd7906e4b8f4c
kyorohiro <kyorohiro@gmail.com>
2015-06-23 09:12:01 +0900

    ..

--- test/bitfield_test.dart
+++ test/bitfield_test.dart
@@ -37,5 +37,37 @@ void main() {
       unit.expect(false, bitfield.getIsOn(0));
       unit.expect(0x00, 0xFF&bitfield.getBinary()[0]);
     });
+
+    unit.test("test_bitsizeIsNine", () {
+      Bitfield bitfield = new Bitfield(9);
+      unit.expect(2, bitfield.getBinary().length);
+      unit.expect(9, bitfield.lengthPerBit());
+      unit.expect(2, bitfield.lengthPerByte());
+      unit.expect(false, bitfield.isAllOff());
+      unit.expect(true, bitfield.isAllOn());
+      unit.expect(0xFF, 0xFF&bitfield.getBinary()[0]);
+      unit.expect(0x80, 0xFF&bitfield.getBinary()[1]);
+
+      for(int i=0;i<bitfield.lengthPerBit();i++){
+        unit.expect(true, bitfield.getIsOn(i));
+      }
+
+      bitfield.setIsOn(1, false);
+      unit.expect(false, bitfield.getIsOn(1));
+
+      bitfield.setIsOn(8, false);
+      unit.expect(false, bitfield.getIsOn(8));
+
+      unit.expect(false, bitfield.isAllOff());
+      unit.expect(false, bitfield.isAllOn());
+
+      bitfield.oneClear();
+      unit.expect(false, bitfield.isAllOff());
+      unit.expect(true, bitfield.isAllOn());
+      
+      bitfield.zeroClear();
+      unit.expect(true, bitfield.isAllOff());
+      unit.expect(false, bitfield.isAllOn());
+    });
   });
 }
