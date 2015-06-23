d5741cae02559f0cb61530b0108c76ed3173b02c
kyorohiro <kyorohiro@gmail.com>
2015-06-23 11:04:13 +0900

    ..

--- lib/hetimatorrent.dart
+++ lib/hetimatorrent.dart
@@ -3,18 +3,17 @@ library hetimatorrent;
 export 'src/util/bencode.dart';
 export 'src/util/hetibencode.dart';
 export 'src/util/bitfield.dart';
+export 'src/util/shufflelinkedlist.dart';
+export 'src/util/peeridcreator.dart';
+
 export 'src/file/torrentfile.dart';
 export 'src/file/torrentfilehelper.dart';
+
 export 'src/tracker/trackerurl.dart';
 export 'src/tracker/trackerrequest.dart';
 export 'src/tracker/trackerresponse.dart';
 export 'src/tracker/trackerpeerinfo.dart';
 export 'src/tracker/trackerclient.dart';
-
-
-
-export 'src/util/shufflelinkedlist.dart';
-export 'src/util/peeridcreator.dart';
 export 'src/tracker/trackerpeermanager.dart';
 export 'src/tracker/trackerserver.dart';
 
@@ -24,4 +23,5 @@ export 'src/client/torrentclientfront.dart';
 
 
 export 'src/extra/terminal.dart';
+
 export 'src/message/message.dart';
--- lib/src/util/blockdata.dart
+++ lib/src/util/blockdata.dart
@@ -2,10 +2,26 @@ library hetimatorrent.torrent.blockdata;
 
 import 'dart:core';
 import 'dart:math';
+import 'dart:async';
+
 import 'package:hetimacore/hetimacore.dart';
+import 'bitfield.dart';
 
 class BlockData {
-  BlockData() {
-    ;
+
+  Bitfield _head;
+  HetimaData _data;
+  int _blockSize;
+  BlockData(HetimaData data, Bitfield head, int blockSize) {
+    _data = data;
+    _head = head;
+    _blockSize = blockSize;
+  }
+
+
+  Future write(List<int> data, int blockNum) {
+    return new Future(() {
+      
+    });
   }
 }
