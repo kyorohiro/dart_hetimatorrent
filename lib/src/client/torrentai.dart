d9d34b11a6b82ad9c9072e70bcbd0addbf319aae
kyorohiro <kyorohiro@gmail.com>
2015-06-24 22:02:35 -0700

    ..

--- lib/src/client/torrentai.dart
+++ lib/src/client/torrentai.dart
@@ -3,37 +3,60 @@ library hetimatorrent.torrent.ai;
 import 'dart:core';
 import 'dart:async';
 import '../message/message.dart';
-
+import 'package:hetimacore/hetimacore.dart';
 import 'torrentclient.dart';
 import 'torrentclientfront.dart';
 import 'torrentclientpeerinfo.dart';
 
 abstract class TorrentAI {
-  Future onReceive(TorrentClient client, TorrentClientPeerInfo info, TorrentMessage message);
-  Future onSignal(TorrentClient client, TorrentClientPeerInfo info, TorrentClientSignal message);
+  Future onReceive(
+      TorrentClient client, TorrentClientPeerInfo info, TorrentMessage message);
+  Future onSignal(TorrentClient client, TorrentClientPeerInfo info,
+      TorrentClientSignal message);
 }
 
 class TorrentAIBasicDelivery extends TorrentAI {
-  Future onReceive(TorrentClient client, TorrentClientPeerInfo info,TorrentMessage message) {
+  Future onReceive(TorrentClient client, TorrentClientPeerInfo info,
+      TorrentMessage message) {
     return new Future(() {
       TorrentClientFront front = info.front;
-      if (message.id == TorrentMessage.DUMMY_SIGN_SHAKEHAND) {
-        MessageHandshake handshakeMessage = message;
-        if (true == front.handshakeToMe) {
-          return true;
-        }
-        if(handshakeMessage.peerId == client.peerId) {
-          front.close();
-          info.amI = true;
-        }
-        return front.sendHandshake();
+      switch (message.id) {
+        case TorrentMessage.DUMMY_SIGN_SHAKEHAND:
+          {
+            MessageHandshake handshakeMessage = message;
+            if (true == front.handshakeToMe) {
+              return true;
+            }
+            if (handshakeMessage.peerId == client.peerId) {
+              front.close();
+              info.amI = true;
+              return null;
+            }
+            return front.sendHandshake();
+          }
+          break;
+        case TorrentMessage.SIGN_REQUEST:
+          {
+            MessageRequest requestMessage = message;
+            int begin = requestMessage.begin;
+            int len = requestMessage.length;
+            
+            return client.targetBlock.read(requestMessage.index).then((ReadResult result) {
+              return front.sendPiece(requestMessage.index, requestMessage.begin,
+                 result.buffer.sublist(begin, begin + len)).then((_){
+                ;
+              });
+            });
+          }
+          break;
       }
     });
   }
-  
-  Future onSignal(TorrentClient client, TorrentClientPeerInfo info, TorrentClientSignal signal) {
-    return new Future((){
-      if(signal.signal == TorrentClientFrontSignal.HANDSHAKED) {
+
+  Future onSignal(TorrentClient client, TorrentClientPeerInfo info,
+      TorrentClientSignal signal) {
+    return new Future(() {
+      if (signal.signal == TorrentClientFrontSignal.HANDSHAKED) {
         info.front.sendBitfield(client.targetBlock.bitfield);
       }
     });
