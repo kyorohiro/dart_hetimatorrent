6ded07bf8886f3e2c9622b4926a0afc366f4dc28
kyorohiro <kyorohiro@gmail.com>
2015-06-26 19:24:12 +0900

    ..

--- example/HetimaTorrentTracker/web/ui_mainview.dart
+++ example/HetimaTorrentTracker/web/ui_mainview.dart
@@ -1,15 +1,13 @@
 library app.mainview;
 
 import 'dart:html' as html;
+import 'package:chrome/chrome_app.dart' as chrome;
 import 'package:hetimacore/hetimacore.dart';
 import 'package:hetimacore/hetimacore_cl.dart';
 import 'package:hetimanet/hetimanet.dart';
-
 import 'package:hetimatorrent/hetimatorrent.dart';
 import 'ui_dialog.dart';
 import 'modeL_tracker.dart';
-
-
 
 class MainItem {
   html.InputElement fileInput = html.querySelector("#fileinput");
@@ -27,8 +25,7 @@ class MainItem {
   html.InputElement inputLocalAddress = html.querySelector("#input-localaddress");
   html.InputElement inputLocalPort = html.querySelector("#input-localport");
   html.InputElement inputGlobalPort = html.querySelector("#input-globalport");
-  
-  
+
   void init(TrackerModel model, Map<String, TorrentFile> managedTorrentFile, Tab tab, Dialog dialog) {
     fileInput.onChange.listen((html.Event e) {
       print("==");
@@ -49,7 +46,7 @@ class MainItem {
         });
       }
     });
-    
+
     startServerBtn.onClick.listen((html.MouseEvent e) {
       loadServerBtn.style.display = "block";
       stopServerBtn.style.display = "none";
@@ -83,7 +80,7 @@ class MainItem {
         loadServerBtn.style.display = "none";
       });
     });
-    
+
     // Adds a click event for each radio button in the group with name "gender"
     html.querySelectorAll('[name="upnpon"]').forEach((html.InputElement radioButton) {
       radioButton.onClick.listen((html.MouseEvent e) {
@@ -111,7 +108,65 @@ class MainItem {
   }
 }
 
+class CreateItem {
+  html.InputElement inputFile = html.querySelector("#create-fileinput");
+  html.InputElement inputAnnounce = html.querySelector("#create-announce");
+  html.InputElement inputPieceLength = html.querySelector("#create-piece-length");
+  html.AnchorElement inputLink = html.querySelector("#create-link");
+  html.File _rawFile = null;
+  Dialog dialog = null;
 
+  init(TrackerModel model, Map<String, TorrentFile> managedTorrentFile, Tab tab, Dialog d) {
+    dialog = d;
+    inputFile.onChange.listen((html.Event e) {
+      print("==");
+      if (inputFile.files.length > 0) {
+        html.File n = inputFile.files[0];
+        TorrentFileCreator cre = new TorrentFileCreator();
+        cre.announce = inputAnnounce.value;
+        cre.piececLength = int.parse(inputPieceLength.value);
+        return cre.createFromSingleFile(new HetimaDataBlob(n)).then((TorrentFileCreatorResult r) {
+          List<int> buffer = Bencode.encode(r.torrentFile.mMetadata);
+          HetimaDataFS fs = new HetimaDataFS("a.torrent");
+          return fs.write(buffer, 0).then((WriteResult r) {
+            return fs.getEntry().then((html.Entry e) {
+              inputLink.href = e.toUrl();
+              (e as html.FileEntry).file().then((html.File f) {
+                _rawFile = f;
+              });
+            });
+          });
+        });
+      }
+    });
+    inputLink.onClick.listen((_) {
+      print("click");
+      saveFile();
+    });
+  }
+
+  void saveFile() {
+    String choseFile = "";
+    try {
+      chrome.fileSystem.chooseEntry(new chrome.ChooseEntryOptions(type: chrome.ChooseEntryType.SAVE_FILE, suggestedName: "a.torrent")).then((chrome.ChooseEntryResult chooseEntryResult) {
+        choseFile = chooseEntryResult.entry.toUrl();
+        chrome.fileSystem.getWritableEntry(chooseEntryResult.entry).then((chrome.ChromeFileEntry copyTo) {
+          HetimaDataBlob copyFrom = new HetimaDataBlob(_rawFile);
+          copyFrom.getLength().then((int length) {
+            copyFrom.read(0, length).then((ReadResult readResult) {
+              chrome.ArrayBuffer buffer = new chrome.ArrayBuffer.fromBytes(readResult.buffer.toList());
+//              copyTo.remove().then((e){
+              copyTo.writeBytes(buffer);
+//              });
+            });
+          });
+        });
+      });
+    } catch (e) {
+      dialog.show("failed to copy");
+    }
+  }
+}
 //
 //
 class HashItem {
@@ -128,8 +183,6 @@ class HashItem {
         model.selectKey = null;
       }
     });
-    
-    
   }
   void contain(TrackerModel model, Map<String, TorrentFile> managedTorrentFile, String key) {
     if (managedTorrentFile.containsKey(key)) {
@@ -145,29 +198,25 @@ class HashItem {
 //
 //
 void main() {
-  Tab tab = new Tab({
-    "#m00_clone": "#com-clone",
-    "#m00_create": "#com-create",
-    });
+  Tab tab = new Tab({"#m00_clone": "#com-clone", "#m00_create": "#com-create"});
   Dialog dialog = new Dialog();
 
   Map<String, TorrentFile> managedTorrentFile = {};
   TrackerModel model = new TrackerModel();
   HashItem item = null;
   MainItem mainImte = null;
-  
+  CreateItem createImte = new CreateItem();
   print("hello world");
   tab.init();
   dialog.init();
   item = new HashItem();
   mainImte = new MainItem();
-
   item.init(model, managedTorrentFile, tab);
   mainImte.init(model, managedTorrentFile, tab, dialog);
+  createImte.init(model, managedTorrentFile, tab, dialog);
 
   tab.onShow.listen((TabInfo info) {
     print("=t= ${info.cont}");
     item.contain(model, managedTorrentFile, info.key);
   });
-
 }
