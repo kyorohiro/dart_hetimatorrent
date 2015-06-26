00b1d472235166ed67b03fa4ee6058868f520082
kyorohiro <kyorohiro@gmail.com>
2015-06-26 16:48:10 +0900

    ..

--- example/HetimaTorrentTracker/web/ui_mainview.dart
+++ example/HetimaTorrentTracker/web/ui_mainview.dart
@@ -10,141 +10,164 @@ import 'ui_dialog.dart';
 import 'modeL_tracker.dart';
 
 
-Tab tab = new Tab({"#m00_clone": "#com-clone"});
-Dialog dialog = new Dialog();
 
-Map<String, TorrentFile> managedTorrentFile = {};
+class MainItem {
+  html.InputElement fileInput = html.querySelector("#fileinput");
+  html.InputElement managedfile = html.querySelector("#managedfile");
 
-html.InputElement fileInput = html.querySelector("#fileinput");
-html.InputElement managedfile = html.querySelector("#managedfile");
+  html.InputElement startServerBtn = html.querySelector("#startserver");
+  html.InputElement stopServerBtn = html.querySelector("#stopserver");
+  html.InputElement loadServerBtn = html.querySelector("#loaderserver");
 
-html.InputElement startServerBtn = html.querySelector("#startserver");
-html.InputElement stopServerBtn = html.querySelector("#stopserver");
-html.InputElement loadServerBtn = html.querySelector("#loaderserver");
+  html.SpanElement outputLocalAddressSpn = html.querySelector("#localaddress");
+  html.SpanElement outputLocalPortSpn = html.querySelector("#localport");
+  html.SpanElement outputGlobalAddressSpn = html.querySelector("#globaladdress");
+  html.SpanElement outputGlobalPortSpn = html.querySelector("#globalport");
 
-html.SpanElement outputLocalAddressSpn = html.querySelector("#localaddress");
-html.SpanElement outputLocalPortSpn = html.querySelector("#localport");
-html.SpanElement outputGlobalAddressSpn = html.querySelector("#globaladdress");
-html.SpanElement outputGlobalPortSpn = html.querySelector("#globalport");
+  html.InputElement inputLocalAddress = html.querySelector("#input-localaddress");
+  html.InputElement inputLocalPort = html.querySelector("#input-localport");
+  html.InputElement inputGlobalPort = html.querySelector("#input-globalport");
+  
+  
+  void init(TrackerModel model, Map<String, TorrentFile> managedTorrentFile, Tab tab, Dialog dialog) {
+    fileInput.onChange.listen((html.Event e) {
+      print("==");
+      List<html.File> s = [];
+      s.addAll(fileInput.files);
+      while (s.length > 0) {
+        html.File n = s.removeAt(0);
+        print("#${n.name} ${e}");
+        TorrentFile.createTorrentFileFromTorrentFile(new HetimaFileToBuilder(new HetimaDataBlob(n))).then((TorrentFile f) {
+          return f.createInfoSha1().then((List<int> infoHash) {
+            String key = PercentEncode.encode(infoHash);
+            managedTorrentFile[key] = f;
+            tab.add("${key}", "con-now");
+            model.onAddInfoHashFromTracker(f);
+          });
+        }).catchError((e) {
+          dialog.show("failed parse torrent");
+        });
+      }
+    });
+    
+    startServerBtn.onClick.listen((html.MouseEvent e) {
+      loadServerBtn.style.display = "block";
+      stopServerBtn.style.display = "none";
+      startServerBtn.style.display = "none";
 
-html.InputElement inputLocalAddress = html.querySelector("#input-localaddress");
-html.InputElement inputLocalPort = html.querySelector("#input-localport");
-html.InputElement inputGlobalPort = html.querySelector("#input-globalport");
+      model.onStart(inputLocalAddress.value, int.parse(inputLocalPort.value), int.parse(inputGlobalPort.value)).then((List<String> v) {
+        outputLocalPortSpn.innerHtml = v[1];
+        outputLocalAddressSpn.innerHtml = v[0];
+        stopServerBtn.style.display = "block";
+        startServerBtn.style.display = "none";
+        loadServerBtn.style.display = "none";
+      }).catchError((e) {
+        stopServerBtn.style.display = "none";
+        startServerBtn.style.display = "block";
+        loadServerBtn.style.display = "none";
+      });
+    });
+
+    stopServerBtn.onClick.listen((html.MouseEvent e) {
+      loadServerBtn.style.display = "block";
+      stopServerBtn.style.display = "none";
+      startServerBtn.style.display = "none";
+
+      model.onStop().then((StopResult r) {
+        startServerBtn.style.display = "block";
+        stopServerBtn.style.display = "none";
+        loadServerBtn.style.display = "none";
+      }).catchError((e) {
+        startServerBtn.style.display = "none";
+        stopServerBtn.style.display = "block";
+        loadServerBtn.style.display = "none";
+      });
+    });
+    
+    // Adds a click event for each radio button in the group with name "gender"
+    html.querySelectorAll('[name="upnpon"]').forEach((html.InputElement radioButton) {
+      radioButton.onClick.listen((html.MouseEvent e) {
+        html.InputElement clicked = e.target;
+        print("The user is ${clicked.value}");
+        if (clicked.value == "Use") {
+          model.upnpIsUse = true;
+        } else {
+          model.upnpIsUse = false;
+        }
+      });
+    });
+
+    model.portMapHelder.onUpdateGlobalIp.listen((String globalIP) {
+      outputGlobalAddressSpn.setInnerHtml(globalIP);
+    });
+
+    model.portMapHelder.onUpdateGlobalPort.listen((String globalPort) {
+      outputGlobalPortSpn.setInnerHtml(globalPort);
+    });
+
+    model.portMapHelder.startGetLocalIp().then((StartGetLocalIPResult result) {
+      inputLocalAddress.value = result.localIP;
+    });
+  }
+}
+
 
 //
 //
-html.SpanElement torrentHashSpan = html.querySelector("#torrent-hash");
-html.SpanElement torrentRemoveBtn = html.querySelector("#torrent-remove-btn");
-html.SpanElement torrentNumOfPeerSpan = html.querySelector("#torrent-num-of-peer");
+class Item {
+  html.SpanElement torrentHashSpan = html.querySelector("#torrent-hash");
+  html.SpanElement torrentRemoveBtn = html.querySelector("#torrent-remove-btn");
+  html.SpanElement torrentNumOfPeerSpan = html.querySelector("#torrent-num-of-peer");
 
+  init(TrackerModel model, Map<String, TorrentFile> managedTorrentFile, Tab tab) {
+    torrentRemoveBtn.onClick.listen((html.MouseEvent e) {
+      if (model.selectKey != null) {
+        tab.remove(model.selectKey);
+        managedTorrentFile.remove(model.selectKey);
+        model.onRemoveInfoHashFromTracker(PercentEncode.decode(model.selectKey));
+        model.selectKey = null;
+      }
+    });
+    
+    
+  }
+  void contain(TrackerModel model, Map<String, TorrentFile> managedTorrentFile, String key) {
+    if (managedTorrentFile.containsKey(key)) {
+      torrentHashSpan.setInnerHtml("${key}");
+      model.selectKey = key;
+      List<int> infoHash = PercentEncode.decode(key);
+      torrentNumOfPeerSpan.setInnerHtml("${model.onGetNumOfPeer(infoHash)}");
+    }
+  }
+}
 
-TrackerModel model = new TrackerModel();
 //
 //
 //
 void main() {
+  Tab tab = new Tab({"#m00_clone": "#com-clone"});
+  Dialog dialog = new Dialog();
+
+  Map<String, TorrentFile> managedTorrentFile = {};
+  TrackerModel model = new TrackerModel();
+  Item item = null;
+  MainItem mainImte = null;
+  
   print("hello world");
   tab.init();
   dialog.init();
+  item = new Item();
+  mainImte = new MainItem();
 
-  torrentRemoveBtn.onClick.listen((html.MouseEvent e) {
-    if (model.selectKey != null) {
-      tab.remove(model.selectKey);
-      managedTorrentFile.remove(model.selectKey);
-      model.onRemoveInfoHashFromTracker(PercentEncode.decode(model.selectKey));
-      model.selectKey = null;
-    }
-  });
+  item.init(model, managedTorrentFile, tab);
+  mainImte.init(model, managedTorrentFile, tab, dialog);
 
-  fileInput.onChange.listen((html.Event e) {
-    print("==");
-    List<html.File> s = [];
-    s.addAll(fileInput.files);
-    while (s.length > 0) {
-      html.File n = s.removeAt(0);
-      print("#${n.name} ${e}");
-      TorrentFile.createTorrentFileFromTorrentFile(new HetimaFileToBuilder(new HetimaDataBlob(n))).then((TorrentFile f) {
-        return f.createInfoSha1().then((List<int> infoHash) {
-          String key = PercentEncode.encode(infoHash);
-          managedTorrentFile[key] = f;
-          tab.add("${key}", "con-now");
-          model.onAddInfoHashFromTracker(f);
-        });
-      }).catchError((e) {
-        dialog.show("failed parse torrent");
-      });
-    }
-  });
 
-  startServerBtn.onClick.listen((html.MouseEvent e) {
-    loadServerBtn.style.display = "block";
-    stopServerBtn.style.display = "none";
-    startServerBtn.style.display = "none";
-
-    model.onStart(inputLocalAddress.value, int.parse(inputLocalPort.value), int.parse(inputGlobalPort.value)).then((List<String> v) {
-      outputLocalPortSpn.innerHtml = v[1];
-      outputLocalAddressSpn.innerHtml = v[0];
-      stopServerBtn.style.display = "block";
-      startServerBtn.style.display = "none";
-      loadServerBtn.style.display = "none";
-    }).catchError((e) {
-      stopServerBtn.style.display = "none";
-      startServerBtn.style.display = "block";
-      loadServerBtn.style.display = "none";
-    });
-  });
-
-  stopServerBtn.onClick.listen((html.MouseEvent e) {
-    loadServerBtn.style.display = "block";
-    stopServerBtn.style.display = "none";
-    startServerBtn.style.display = "none";
-
-    model.onStop().then((StopResult r) {
-      startServerBtn.style.display = "block";
-      stopServerBtn.style.display = "none";
-      loadServerBtn.style.display = "none";
-    }).catchError((e) {
-      startServerBtn.style.display = "none";
-      stopServerBtn.style.display = "block";
-      loadServerBtn.style.display = "none";
-    });
-  });
 
   tab.onShow.listen((TabInfo info) {
-    String t = info.cont;
-    print("=t= ${t}");
-
-    String key = info.key;
-    if (managedTorrentFile.containsKey(key)) {
-      torrentHashSpan.setInnerHtml("${info.key}");
-      model.selectKey = key;
-      List<int> infoHash = PercentEncode.decode(info.key);
-      torrentNumOfPeerSpan.setInnerHtml("${model.onGetNumOfPeer(infoHash)}");
-    }
+    print("=t= ${info.cont}");
+    item.contain(model, managedTorrentFile, info.key);
   });
 
-  // Adds a click event for each radio button in the group with name "gender"
-  html.querySelectorAll('[name="upnpon"]').forEach((html.InputElement radioButton) {
-    radioButton.onClick.listen((html.MouseEvent e) {
-      html.InputElement clicked = e.target;
-      print("The user is ${clicked.value}");
-      if (clicked.value == "Use") {
-        model.upnpIsUse = true;
-      } else {
-        model.upnpIsUse = false;
-      }
-    });
-  });
 
-  model.portMapHelder.onUpdateGlobalIp.listen((String globalIP) {
-    outputGlobalAddressSpn.setInnerHtml(globalIP);
-  });
-
-  model.portMapHelder.onUpdateGlobalPort.listen((String globalPort) {
-    outputGlobalPortSpn.setInnerHtml(globalPort);
-  });
-
-  model.portMapHelder.startGetLocalIp().then((StartGetLocalIPResult result) {
-    inputLocalAddress.value = result.localIP;
-  });
 }
