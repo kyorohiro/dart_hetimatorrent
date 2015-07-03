09c635c8aa85407eebe18d44ac03ae9238d11cd7
kyorohiro <kyorohiro@gmail.com>
2015-07-03 23:00:37 +0900

    ..

--- example/HetimaTorrentTracker/web/model_seeder.dart
+++ example/HetimaTorrentTracker/web/model_seeder.dart
@@ -2,21 +2,39 @@ library app.mainview.model.seeder;
 
 import 'dart:async';
 import 'dart:html' as html;
-import 'package:chrome/chrome_app.dart' as chrome;
 import 'package:hetimacore/hetimacore.dart';
-import 'package:hetimacore/hetimacore_cl.dart';
 import 'package:hetimanet/hetimanet.dart';
 import 'package:hetimanet/hetimanet_chrome.dart';
 import 'package:hetimatorrent/hetimatorrent.dart';
 
 class SeederModel {
   TorrentClient client = null;
-  
-  init(TorrentFile torrentFile, HetimaData seed) {
-    TorrentClient.create(new HetiSocketBuilderChrome(),
-        PeerIdCreator.createPeerid("seeder"),
-        torrentFile, seed);
+  UpnpPortMapHelper portMapHelder = new UpnpPortMapHelper(new HetiSocketBuilderChrome(), "HetimaTorrentTracker");
 
-    
+  String localIp = "0.0.0.0";
+  int localPort = 18080;
+  int globalPort = 18080;
+  bool useUpnp = false;
+  
+  Future start(TorrentFile torrentFile, HetimaData seed) {
+    return TorrentClient.create(new HetiSocketBuilderChrome(),
+        PeerIdCreator.createPeerid("seeder"),
+        torrentFile, seed).then((TorrentClient client){
+      client.localAddress = localIp;
+      client.port = localPort;
+      return client.start();
+    }).then((_){
+      if(useUpnp == true) {
+        portMapHelder.basePort = globalPort;
+        portMapHelder.numOfRetry = 0;
+        return portMapHelder.startPortMap().then((StartPortMapResult r) {
+          return null;
+        });
+      } else {
+        return new Future(() {
+          return null;
+        });
+      }
+    });
   }
 }
