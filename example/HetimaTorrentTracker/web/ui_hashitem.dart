0293c58b18f55624b4dbad67858ea600be295f84
kyorohiro <kyorohiro@gmail.com>
2015-07-03 17:49:47 +0900

    ..

---
+++ example/HetimaTorrentTracker/web/ui_hashitem.dart
@@ -0,0 +1,34 @@
+library app.mainview.hashitem;
+
+import 'dart:html' as html;
+import 'package:hetimacore/hetimacore.dart';
+import 'package:hetimatorrent/hetimatorrent.dart';
+import 'ui_dialog.dart';
+import 'model_tracker.dart';
+
+//
+//
+class HashItem {
+  html.SpanElement torrentHashSpan = html.querySelector("#torrent-hash");
+  html.SpanElement torrentRemoveBtn = html.querySelector("#torrent-remove-btn");
+  html.SpanElement torrentNumOfPeerSpan = html.querySelector("#torrent-num-of-peer");
+
+  init(TrackerModel model, Map<String, TorrentFile> managedTorrentFile, Tab tab) {
+    torrentRemoveBtn.onClick.listen((html.MouseEvent e) {
+      if (model.selectKey != null) {
+        tab.remove(model.selectKey);
+        managedTorrentFile.remove(model.selectKey);
+        model.onRemoveInfoHashFromTracker(PercentEncode.decode(model.selectKey));
+        model.selectKey = null;
+      }
+    });
+  }
+  void contain(TrackerModel model, Map<String, TorrentFile> managedTorrentFile, String key) {
+    if (managedTorrentFile.containsKey(key)) {
+      torrentHashSpan.setInnerHtml("${key}");
+      model.selectKey = key;
+      List<int> infoHash = PercentEncode.decode(key);
+      torrentNumOfPeerSpan.setInnerHtml("${model.onGetNumOfPeer(infoHash)}");
+    }
+  }
+}
--- example/HetimaTorrentTracker/web/ui_mainview.dart
+++ example/HetimaTorrentTracker/web/ui_mainview.dart
@@ -1,13 +1,14 @@
 library app.mainview;
 
 import 'dart:html' as html;
-import 'package:chrome/chrome_app.dart' as chrome;
 import 'package:hetimacore/hetimacore.dart';
 import 'package:hetimacore/hetimacore_cl.dart';
 import 'package:hetimanet/hetimanet.dart';
 import 'package:hetimatorrent/hetimatorrent.dart';
 import 'ui_dialog.dart';
 import 'model_tracker.dart';
+import 'ui_createitem.dart';
+import 'ui_hashitem.dart';
 
 class MainItem {
   html.InputElement fileInput = html.querySelector("#fileinput");
@@ -108,112 +109,7 @@ class MainItem {
   }
 }
 
-class CreateItem {
-  html.InputElement inputFile = html.querySelector("#create-fileinput");
-  html.InputElement inputAnnounce = html.querySelector("#create-announce");
-  html.InputElement inputPieceLength = html.querySelector("#create-piece-length");
-  html.AnchorElement inputLink = html.querySelector("#create-link");
 
-  html.InputElement inputCacheSize = html.querySelector("#create-cache-size");
-  html.InputElement inputThreadNum = html.querySelector("#create-thread-num");
-  html.SpanElement outputProgress = html.querySelector("#create-progress");
-  
-  html.File _rawFile = null;
-  Dialog dialog = null;
-
-
-  init(TrackerModel model, Map<String, TorrentFile> managedTorrentFile, Tab tab, Dialog d) {
-    dialog = d;
-    inputFile.onChange.listen((html.Event e) {
-      print("==");
-      inputLink.style.display = "none";
-      if (inputFile.files.length > 0) {
-
-        html.File n = inputFile.files[0];
-        TorrentFileCreator cre = new TorrentFileCreator();
-        cre.announce = inputAnnounce.value;
-        cre.piececLength = int.parse(inputPieceLength.value) * 1024;
-        
-        int cashSize = int.parse(inputCacheSize.value)*1024;
-        if(cashSize > 0 && cashSize < cre.piececLength) {
-          cashSize = cashSize*2;
-        }
-        int threadNum = int.parse(inputThreadNum.value);
-        onPro(int v) {
-          outputProgress.setInnerHtml("${v} / ${n.size}");
-        }
-        return cre.createFromSingleFile(new HetimaDataBlob(n),
-             threadNum:threadNum-1, cacheSize: cashSize, cacheNum: 3, progress:onPro,isopath:"subiso.dart").then((TorrentFileCreatorResult r) {
-          List<int> buffer = Bencode.encode(r.torrentFile.mMetadata);
-          HetimaDataFS fs = new HetimaDataFS("a.torrent");
-          return fs.write(buffer, 0).then((WriteResult r) {
-            return fs.truncate(buffer.length).then((_) {
-              return fs.getEntry().then((html.Entry e) {
-                inputLink.href = e.toUrl();
-                inputLink.style.display = "block";
-                (e as html.FileEntry).file().then((html.File f) {
-                  _rawFile = f;
-                });
-              });
-            });
-          });
-        });
-      }
-    });
-    inputLink.onClick.listen((_) {
-      print("click");
-      saveFile();
-    });
-  }
-
-  void saveFile() {
-    String choseFile = "";
-    try {
-      chrome.fileSystem.chooseEntry(new chrome.ChooseEntryOptions(type: chrome.ChooseEntryType.SAVE_FILE, suggestedName: "a.torrent")).then((chrome.ChooseEntryResult chooseEntryResult) {
-        choseFile = chooseEntryResult.entry.toUrl();
-        chrome.fileSystem.getWritableEntry(chooseEntryResult.entry).then((chrome.ChromeFileEntry copyTo) {
-          HetimaDataBlob copyFrom = new HetimaDataBlob(_rawFile);
-          copyFrom.getLength().then((int length) {
-            copyFrom.read(0, length).then((ReadResult readResult) {
-              chrome.ArrayBuffer buffer = new chrome.ArrayBuffer.fromBytes(readResult.buffer.toList());
-//              copyTo.remove().then((e){
-              copyTo.writeBytes(buffer);
-//              });
-            });
-          });
-        });
-      });
-    } catch (e) {
-      dialog.show("failed to copy");
-    }
-  }
-}
-//
-//
-class HashItem {
-  html.SpanElement torrentHashSpan = html.querySelector("#torrent-hash");
-  html.SpanElement torrentRemoveBtn = html.querySelector("#torrent-remove-btn");
-  html.SpanElement torrentNumOfPeerSpan = html.querySelector("#torrent-num-of-peer");
-
-  init(TrackerModel model, Map<String, TorrentFile> managedTorrentFile, Tab tab) {
-    torrentRemoveBtn.onClick.listen((html.MouseEvent e) {
-      if (model.selectKey != null) {
-        tab.remove(model.selectKey);
-        managedTorrentFile.remove(model.selectKey);
-        model.onRemoveInfoHashFromTracker(PercentEncode.decode(model.selectKey));
-        model.selectKey = null;
-      }
-    });
-  }
-  void contain(TrackerModel model, Map<String, TorrentFile> managedTorrentFile, String key) {
-    if (managedTorrentFile.containsKey(key)) {
-      torrentHashSpan.setInnerHtml("${key}");
-      model.selectKey = key;
-      List<int> infoHash = PercentEncode.decode(key);
-      torrentNumOfPeerSpan.setInnerHtml("${model.onGetNumOfPeer(infoHash)}");
-    }
-  }
-}
 
 //
 //
