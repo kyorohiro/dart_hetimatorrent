db98cb305bf4ab91eeb588892b9c9fc5830914b2
kyorohiro <kyorohiro@gmail.com>
2015-07-03 17:59:20 +0900

    ..

---
+++ example/HetimaTorrentTracker/web/model_main.dart
@@ -0,0 +1,38 @@
+library app.mainview;
+
+import 'package:hetimatorrent/hetimatorrent.dart';
+import 'ui_dialog.dart';
+import 'model_tracker.dart';
+import 'ui_createitem.dart';
+import 'ui_hashitem.dart';
+import 'ui_mainview.dart';
+
+
+//
+//
+//
+void main() {
+  Tab tab = new Tab({"#m00_clone": "#com-clone", "#m00_create": "#com-create"});
+  Dialog dialog = new Dialog();
+
+  Map<String, TorrentFile> managedTorrentFile = {};
+  TrackerModel model = new TrackerModel();
+  HashItem item = null;
+  MainItem mainImte = null;
+  CreateItem createImte = new CreateItem();
+  print("hello world");
+  tab.init();
+  dialog.init();
+  item = new HashItem();
+  mainImte = new MainItem();
+  item.init(model, managedTorrentFile, tab);
+  mainImte.init(model, managedTorrentFile, tab, dialog);
+  createImte.init(model, managedTorrentFile, tab, dialog);
+
+  tab.onShow.listen((TabInfo info) {
+    print("=t= ${info.cont}");
+    item.contain(model, managedTorrentFile, info.key);
+  });
+}
+
+
--- example/HetimaTorrentTracker/web/ui_mainview.dart
+++ example/HetimaTorrentTracker/web/ui_mainview.dart
@@ -110,32 +110,3 @@ class MainItem {
 }
 
 
-
-//
-//
-//
-void main() {
-  Tab tab = new Tab({"#m00_clone": "#com-clone", "#m00_create": "#com-create"});
-  Dialog dialog = new Dialog();
-
-  Map<String, TorrentFile> managedTorrentFile = {};
-  TrackerModel model = new TrackerModel();
-  HashItem item = null;
-  MainItem mainImte = null;
-  CreateItem createImte = new CreateItem();
-  print("hello world");
-  tab.init();
-  dialog.init();
-  item = new HashItem();
-  mainImte = new MainItem();
-  item.init(model, managedTorrentFile, tab);
-  mainImte.init(model, managedTorrentFile, tab, dialog);
-  createImte.init(model, managedTorrentFile, tab, dialog);
-
-  tab.onShow.listen((TabInfo info) {
-    print("=t= ${info.cont}");
-    item.contain(model, managedTorrentFile, info.key);
-  });
-}
-
-
