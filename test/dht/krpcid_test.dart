6e52b3007066c41a3232e2caa9ac6f3b3e66b0b2
kyorohiro <kyorohiro@gmail.com>
2015-07-20 07:08:08 +0900

    add krpcid test xor

--- test/dht/krpcid_test.dart
+++ test/dht/krpcid_test.dart
@@ -8,17 +8,26 @@ import 'dart:convert' as convert;
 
 void main() {
   unit.group('A group of tests', () {
-    unit.test("bencode: string", () {
+    unit.test("> >= < <=", () {
       KrpcId idA = new KrpcId(new List.filled(20, 0));
       KrpcId idB = new KrpcId(new List.filled(20, 1));
       unit.expect(true, idB > idA);
       unit.expect(true, idB >= idA);
       unit.expect(false, idB < idA);
       unit.expect(false, idB <= idA);
+     //
       unit.expect(false, idB > idB);
       unit.expect(true, idB >= idB);
       unit.expect(false, idB < idB);
       unit.expect(true, idB <= idB);
+    });
+    
+    unit.test("xor", () {
+      KrpcId idA = new KrpcId(new List.filled(20, 255));
+      KrpcId idB = new KrpcId(new List.filled(20, 1));
+      KrpcId idC = new KrpcId(new List.filled(20, 254));
+
+      unit.expect(true, idC == idB.xor(idA));
     });
   });
 }
