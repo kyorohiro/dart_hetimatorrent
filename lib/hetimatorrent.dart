76f31b16a734df991867a7e77baebaf4405a8af8
kyorohiro <kyorohiro@gmail.com>
2015-07-22 03:18:19 +0900

    rename krootingtable

--- lib/hetimatorrent.dart
+++ lib/hetimatorrent.dart
@@ -40,7 +40,7 @@ export 'src/dht/kadid.dart';
 export 'src/dht/message/krpcfindnode.dart';
 export 'src/dht/message/krpcannounce.dart';
 export 'src/dht/message/krpcgetpeers.dart';
-export 'src/dht/rootingtable.dart';
+export 'src/dht/krootingtable.dart';
 export 'src/dht/kpeerinfo.dart';
 
 
