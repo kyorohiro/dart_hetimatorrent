f5ef0313ebc02406efa701036b1d0216cc7c04d8
kyorohiro <kyorohiro@gmail.com>
2015-07-21 20:31:55 +0900

    add kbucket define

--- lib/src/dht/rootingtable.dart
+++ lib/src/dht/rootingtable.dart
@@ -6,10 +6,18 @@ import 'dart:math';
 
 
 class KBucket {
-  
+  int k = 20;
+  List<KPeerInfo> peerInfos = [];
+
+  KBucket(int k_bucketSize) {
+    this.k = k;
+  }
 }
 
+class KPeerInfo {
+  
+}
 class RootingTable {
-
+  List<KBucket> kBuckets = [];
 }
 
