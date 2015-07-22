c38ac49069a0620ef670c39eaaa6905a41b4404b
kyorohiro <kyorohiro@gmail.com>
2015-07-22 23:15:19 +0900

    add knode comm interface

--- lib/src/dht/knode.dart
+++ lib/src/dht/knode.dart
@@ -4,5 +4,29 @@ import 'dart:core';
 import 'dart:async';
 import 'dart:math';
 
+class KNodeComm {
+  Future start(String ip, int port);
+  Future stop();
+  Future write(List<int> bytes);
+  Future read(List<int> bytes);
+}
+
+class KNodeCommSimulator {
+
+  Future start(String ip, int port) {
+    ;
+  }
+  Future stop() {
+    ;
+  }
+  Future write(List<int> bytes) {
+    ;
+  }
+  Future read(List<int> bytes) {
+    ;
+  }
+}
+
 class KNode {
+  
 }
