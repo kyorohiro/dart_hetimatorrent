06eec61e23e7b35053af5b78bd264eff49c775c0
kyorohiro <kyorohiro@gmail.com>
2015-07-23 11:36:06 +0900

    add kNode

--- lib/src/dht/knode.dart
+++ lib/src/dht/knode.dart
@@ -5,7 +5,30 @@ import 'dart:async';
 import 'dart:math';
 import 'package:hetimanet/hetimanet.dart';
 
+class KNode {
+  HetiSocketBuilder _socketBuilder = null;
+  HetiUdpSocket _udpSocket = null;
 
+  KNode(HetiSocketBuilder socketBuilder) {
+    this._socketBuilder = socketBuilder;
+  }
 
+  Future start({String ip: "0.0.0.0", int port: 28080}) {
+    return new Future(() {
+      if (_udpSocket != null) {
+        throw {};
+      }
+      _udpSocket = this._socketBuilder.createUdpClient();
+      return _udpSocket.bind(ip, port);
+    });
+  }
 
-class KNode {}
+  Future stop() {
+    return new Future(() {
+      if (_udpSocket == null) {
+        return null;
+      }
+      return _udpSocket.close();
+    });
+  }
+}
