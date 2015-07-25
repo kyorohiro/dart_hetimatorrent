899b290537244e54cdd7052a4ddaaffe875c91ba
kyorohiro <kyorohiro@gmail.com>
2015-07-25 09:30:55 +0900

    add announceinfo test

--- test/dht/kpeerinfo_test.dart
+++ test/dht/kpeerinfo_test.dart
@@ -11,7 +11,15 @@ import 'dart:async';
 void main() {
   unit.group('A group of tests', () {
     unit.test("retrive list 0", () {
+      KPeerInfo peerInfoA = new KPeerInfo("127.0.0.1", 8080, new KId(new List.filled(20, 1)));
+      KAnnounceInfo announceInfoA = new KAnnounceInfo(peerInfoA, new List.filled(20, 1), 1);
       
+
+      KPeerInfo peerInfoB = new KPeerInfo("127.0.0.1", 8080, new KId(new List.filled(20, 1)));
+      KAnnounceInfo announceInfoB = new KAnnounceInfo(peerInfoB, new List.filled(20, 1), 1);
+
+      unit.expect(peerInfoA, peerInfoB);
+      unit.expect(announceInfoA, announceInfoB);
     });
   });
 }
