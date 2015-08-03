07511811ebc7a1502baee387e74ce96337bebd05
kyorohiro <kyorohiro@gmail.com>
2015-08-03 16:51:07 +0900

    add TorrentDHT Sample

--- example/TorrentDHT/web/torrentdht.dart
+++ example/TorrentDHT/web/torrentdht.dart
@@ -1,15 +1,13 @@
-import 'dart:html';
 import 'dart:async';
-
-import 'package:chrome/chrome_app.dart' as chrome;
 import 'package:hetimatorrent/hetimatorrent.dart';
 import 'package:hetimanet/hetimanet_chrome.dart';
-import 'package:hetimacore/hetimacore_cl.dart';
-import 'package:hetimacore/hetimacore.dart';
 
 void main() {
   KNode node = new KNode(new HetiSocketBuilderChrome());
   node.start().then((_) {
+    String initialNodeIP = "0.0.0.0";
+    int initialNodePort = 14803;
+    node.addNodeFromIPAndPort(initialNodeIP, initialNodePort);
     int torrentClientAccessPort = 18080;
     KId torrentClientDownloadDataHash = new KId(new List.filled(20, 1));
     node.startSearchPeer(torrentClientDownloadDataHash, torrentClientAccessPort);
