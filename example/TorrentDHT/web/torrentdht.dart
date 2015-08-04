9bb4c2bf8cd13f484949346d28598db96bd832d0
kyorohiro <kyorohiro@gmail.com>
2015-08-04 11:18:41 +0900

    ..

--- example/TorrentDHT/web/torrentdht.dart
+++ example/TorrentDHT/web/torrentdht.dart
@@ -12,7 +12,9 @@ void main() {
     int torrentClientAccessPort = 18080;
     KId torrentClientDownloadDataHash = new KId(new List.filled(20, 1));
     node.startSearchPeer(torrentClientDownloadDataHash, torrentClientAccessPort);
-
+    node.onGetPeerValue.listen((KGetPeerValue v) {
+      print("---onGetPeerValue ${v.ipAsString} ${v.port} ${v.infoHashAsString} ");
+    });
     return new Future.delayed(new Duration(seconds: 60 * 5));
   }).then((_) {
     return node.stop();
--- test/dht/knode_002_test.dart
+++ test/dht/knode_002_test.dart
@@ -34,6 +34,9 @@ void main() {
       KId valueInfoHash = KId.createIDAtRandom();
       return new Future.delayed(new Duration(seconds: 3)).then((_) {
         knodes[2].startSearchPeer(valueInfoHash,39999);
+        knodes[2].onGetPeerValue.listen((KGetPeerValue v) {
+          print("---onGetPeerValue ${v.ipAsString} ${v.port} ${v.infoHashAsString} ----------------------------------------");
+        });
         return new Future.delayed(new Duration(seconds: 5));
       }).then((_) {
         knodes[numOfNode ~/ 2].startSearchPeer(valueInfoHash,38888);
