3d7224c416329c6e8a851d6221ebe3a658e2a717
kyorohiro <kyorohiro@gmail.com>
2015-08-05 13:37:54 +0900

    rename ..

--- lib/src/dht/ai/knodeai.dart
+++ lib/src/dht/ai/knodeai.dart
@@ -4,24 +4,11 @@ import 'dart:core';
 import 'dart:async';
 import 'package:hetimacore/hetimacore.dart';
 import 'package:hetimanet/hetimanet.dart';
-import '../krootingtable.dart';
-
-import '../message/krpcping.dart';
-import '../message/krpcfindnode.dart';
-import '../message/krpcgetpeers.dart';
 import '../kid.dart';
 import 'dart:convert';
-import '../../util/shufflelinkedlist.dart';
 
 import '../message/krpcmessage.dart';
-import '../message/krpcping.dart';
-import '../message/krpcfindnode.dart';
-import '../message/krpcgetpeers.dart';
-import '../message/krpcannounce.dart';
-import '../kpeerinfo.dart';
 import '../knode.dart';
-import 'knodeaifindnode.dart';
-import 'knodeiannounce.dart';
 
 abstract class KNodeAI {
   bool get isStart;
