c41c390b7d0394e44ef387aea55a6ca60262ede6
kyorohiro <kyorohiro@gmail.com>
2015-08-10 23:16:54 +0900

    refactring kid

--- lib/src/dht/kid.dart
+++ lib/src/dht/kid.dart
@@ -3,35 +3,30 @@ library hetimatorrent.dht.krpcid;
 import 'dart:core';
 import 'dart:math';
 import 'package:hetimacore/hetimacore.dart';
+import 'dart:typed_data';
 
 class KId {
-  List<int> _id = [];
-  List<int> get id => new List.from(_id);
-  String get idAsString => PercentEncode.encode(_id);
+  List<int> _values = null;
+  List<int> get id => new List.from(_values);
+  String get idAsString => PercentEncode.encode(_values);
 
   KId(List<int> id) {
-    if(id == null || id.length != 20) {
+    if (id == null || id.length != 20) {
       throw {};
     }
-    this._id.addAll(id);
+    this._values = new Uint8List.fromList(id);
   }
 
-  /// 159 is long distance
-  /// 0 is short distance
-  KId.createFromRootingTabkeIndex(int tableIndex) {
-    List<int> p = new List.filled(20, 0);
-    int indexPerByte = tableIndex ~/ 8;
-    int inputPerByte = 0x01 << (tableIndex % 8);
-    p[19 - indexPerByte] = inputPerByte;
-    this._id.addAll(p);
+  KId.zeroClear() {
+    this._values = new Uint8List.fromList(new List.filled(20, 0));
   }
 
   int getRootingTabkeIndex() {
     int ret = 0;
-    for (int i = 0,ret=19; i < 20; i++, ret--) {
-      if (_id[i] != 0) {
+    for (int i = 0, ret = 19; i < 20; i++, ret--) {
+      if (_values[i] != 0) {
         for (int j = 0; j < 9; j++) {
-          if (_id[i] < (0x1 << j)) {
+          if (_values[i] < (0x1 << j)) {
             return (ret * 8) + j;
           }
         }
@@ -43,25 +38,27 @@ class KId {
 
   static KId _optionTest = new KId(new List.filled(20, 0x7f));
   static List<int> createToken(KId infoHash, KId targetId, KId myId, [KId option = null]) {
-    if(option == null) {
+    if (option == null) {
       option = _optionTest;
     }
-    return infoHash.xor(targetId).xor(myId).xor(option)._id;
+    return infoHash.xor(targetId).xor(myId).xor(option)._values;
   }
 
-  KId xor(KId b) {
-    List<int> ret = [];
-    for (int i = 0; i < b._id.length; i++) {
-      ret.add(this._id[i] ^ b._id[i]);
+  KId xor(KId b, [KId output = null]) {
+    if (output == null) {
+      output = new KId.zeroClear();
     }
-    return new KId(ret);
+    for (int i = 0; i < b._values.length; i++) {
+      output._values[i] = this._values[i] ^ b._values[i];
+    }
+    return output;
   }
 
   bool operator >(KId b) {
-    for (int i = 0; i < b._id.length; i++) {
-      if (this._id[i] == b._id[i]) {
+    for (int i = 0; i < b._values.length; i++) {
+      if (this._values[i] == b._values[i]) {
         continue;
-      } else if (this._id[i] > b._id[i]) {
+      } else if (this._values[i] > b._values[i]) {
         return true;
       } else {
         return false;
@@ -71,69 +68,53 @@ class KId {
   }
 
   bool operator ==(KId b) {
-    for (int i = 0; i < b._id.length; i++) {
-      if (this._id[i] != b._id[i]) {
+    for (int i = 0; i < b._values.length; i++) {
+      if (this._values[i] != b._values[i]) {
         return false;
       }
     }
     return true;
   }
 
-  int get hashCode {
-    int v = 0;
-    v = ByteOrder.parseLong(_id, 0, ByteOrder.BYTEORDER_BIG_ENDIAN);
-    v ^= ByteOrder.parseLong(_id, 8, ByteOrder.BYTEORDER_BIG_ENDIAN);
-    v ^= ByteOrder.parseShort(_id, 16, ByteOrder.BYTEORDER_BIG_ENDIAN);
-    return v;
-  }
-
   bool operator >=(KId b) {
-    if (this == b) {
-      return true;
-    } else if (this > b) {
-      return true;
-    } else {
-      return false;
-    }
+    return (this == b ? true : (this > b ? true : false));
   }
 
   bool operator <(KId b) {
-    if (this == b) {
-      return false;
-    } else {
-      return !(this > b);
-    }
+    return (this == b ? false : !(this > b));
   }
 
   bool operator <=(KId b) {
-    if (this == b) {
-      return true;
-    } else if (this > b) {
-      return false;
-    } else {
-      return true;
-    }
+    return (this == b ? true : (this > b ? false : true));
+  }
+
+  int get hashCode {
+    int v = 0;
+    v = ByteOrder.parseLong(_values, 0, ByteOrder.BYTEORDER_BIG_ENDIAN);
+    v ^= ByteOrder.parseLong(_values, 8, ByteOrder.BYTEORDER_BIG_ENDIAN);
+    v ^= ByteOrder.parseShort(_values, 16, ByteOrder.BYTEORDER_BIG_ENDIAN);
+    return v;
   }
 
   String toString() {
     StringBuffer buffer = new StringBuffer();
-    for(int i in _id) {
+    for (int i in _values) {
       String a = i.toRadixString(16);
-      if(a.length ==1) {
+      if (a.length == 1) {
         buffer.write("0");
-      } 
+      }
       buffer.write("${a}.");
     }
     return buffer.toString();
   }
 
-  Iterator<int> get iterator => _id.iterator;
+  Iterator<int> get iterator => _values.iterator;
 
-  static int _i =1000;
+  static int _i = 1000;
   static KId createIDAtRandom([List<int> op = null]) {
     List<int> ret = [];
 
-    Random r = new Random(new DateTime.now().millisecondsSinceEpoch+(_i++));
+    Random r = new Random(new DateTime.now().millisecondsSinceEpoch + (_i++));
     for (int i = 0; i < 20; i++) {
       int v = 0xff;
       if (op != null && i < op.length) {
