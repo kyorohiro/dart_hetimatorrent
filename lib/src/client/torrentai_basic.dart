2bf45d51fe43390ac52621e71134d3b967a0da2d
kyorohiro <kyorohiro@gmail.com>
2015-08-25 20:17:32 +0900

    ..

--- lib/src/client/torrentai_basic.dart
+++ lib/src/client/torrentai_basic.dart
@@ -33,127 +33,107 @@ class TorrentAIBasic extends TorrentAI {
     this._dhtPort = dhtPort;
   }
 
-  Future onTick(TorrentClient client) {
-    return new Future(() {
-      {
-        bool haveAll = client.targetBlock.haveAll();
-        List<TorrentClientPeerInfo> infos = client.rawPeerInfos.getPeerInfo((TorrentClientPeerInfo info) {
-          if (info.front == null || info.front.isClose) {
-            return false;
+  Future onTick(TorrentClient client) async {
+    bool haveAll = client.targetBlock.haveAll();
+    List<TorrentClientPeerInfo> infos = client.rawPeerInfos.getPeerInfo((TorrentClientPeerInfo info) {
+      if (info.front == null || info.front.isClose) {
+        return false;
+      }
+      if (info.amI) {
+        return true;
+      }
+      if (haveAll == true && info.front.bitfieldToMe.isAllOn()) {
+        return true;
+      }
+      return false;
+    });
+    //
+    // close
+    //
+    for (TorrentClientPeerInfo info in infos) {
+      new Future.delayed(new Duration(seconds: 5)).then((_) {
+        info.front.close();
+      });
+    }
+    _chokeTest.chokeTest(client, _maxUnchoke);
+  }
+
+  Future onReceive(TorrentClient client, TorrentClientPeerInfo info, TorrentMessage message) async {
+    TorrentClientFront front = info.front;
+    switch (message.id) {
+      case TorrentMessage.DUMMY_SIGN_SHAKEHAND:
+        if (true == front.handshakeFromMe || info.amI == true) {
+          return null;
+        } else {
+          await front.sendHandshake();
+          if (useDht == true) {
+            if ((message as MessageHandshake).reserved[7] & 0x01 == 0x01) {
+              if (_dhtPort == null) {
+                front.sendPort(client.globalPort);
+              } else {
+                front.sendPort(_dhtPort);
+              }
+            }
           }
-          if (info.amI) {
-            return true;
-          }
-          if (haveAll == true && info.front.bitfieldToMe.isAllOn()) {
-            return true;
-          }
-          return false;
-        });
-        //
-        // close
-        //
-        for (TorrentClientPeerInfo info in infos) {
-          new Future.delayed(new Duration(seconds: 5)).then((_) {
-            info.front.close();
-          });
         }
-      }
-      _chokeTest.chokeTest(client, _maxUnchoke);
-    });
+        break;
+      case TorrentMessage.SIGN_REQUEST:
+        if (info.front.chokedFromMe == TorrentClientFront.STATE_ON) {
+          print("wearn ; already choked ${info.id}");
+          break;
+        }
+
+        MessageRequest requestMessage = message;
+        int index = requestMessage.index;
+        int begin = requestMessage.begin;
+        int len = requestMessage.length;
+
+        if (false == client.targetBlock.have(index)) {
+          front.close();
+          return null;
+        } else {
+          ReadResult result = await client.targetBlock.readBlock(index);
+          List cont = new List.filled(len, 0);
+          if (len > result.buffer.length) {
+            len = result.buffer.length;
+          }
+          cont.setRange(0, len, result.buffer, begin);
+          await front.sendPiece(index, begin, cont);
+        }
+        break;
+      case TorrentMessage.SIGN_BITFIELD:
+      //
+      // targetBlock 'does not reflect. check ID_SET_PIECE_A_PART;
+      // case TorrentMessage.SIGN_PIECE:
+      case TorrentMessage.SIGN_UNCHOKE:
+        if (_pieceTest == null) {
+          _pieceTest = new PieceTest(client);
+        }
+        _pieceTest.pieceTest(client, front);
+        break;
+      case TorrentMessage.SIGN_PORT:
+        break;
+    }
   }
 
-  Future onReceive(TorrentClient client, TorrentClientPeerInfo info, TorrentMessage message) {
-    return new Future(() {
-      TorrentClientFront front = info.front;
-      switch (message.id) {
-        case TorrentMessage.DUMMY_SIGN_SHAKEHAND:
-          {
-            MessageHandshake handshake = message;
-            if (true == front.handshakeFromMe || info.amI == true) {
-              return null;
-            } else {
-              return front.sendHandshake().then((_){
-                if (useDht == true) {
-                  if (handshake.reserved[7] & 0x01 == 0x01) {
-                    if (_dhtPort == null) {
-                      front.sendPort(client.globalPort);
-                    } else {
-                      front.sendPort(_dhtPort);
-                    }
-                  }
-                }
-              });
-            }
-          }
-          break;
-        case TorrentMessage.SIGN_REQUEST:
-          {
-            if (info.front.chokedFromMe == TorrentClientFront.STATE_ON) {
-              print("wearn ; already choked ${info.id}");
-              break;
-            }
-
-            MessageRequest requestMessage = message;
-            int index = requestMessage.index;
-            int begin = requestMessage.begin;
-            int len = requestMessage.length;
-
-            if (false == client.targetBlock.have(index)) {
-              //
-              //
-              front.close();
-              return null;
-            } else {
-              return client.targetBlock.readBlock(index).then((ReadResult result) {
-                List cont = new List.filled(len, 0);
-                if (len > result.buffer.length) {
-                  len = result.buffer.length;
-                }
-                cont.setRange(0, len, result.buffer, begin);
-                return front.sendPiece(index, begin, cont).then((_) {
-                  ;
-                });
-              });
-            }
-          }
-          break;
-        case TorrentMessage.SIGN_BITFIELD:
-        //
-        // targetBlock 'does not reflect. check ID_SET_PIECE_A_PART;
-        // case TorrentMessage.SIGN_PIECE:
-        case TorrentMessage.SIGN_UNCHOKE:
-          if (_pieceTest == null) {
-            _pieceTest = new PieceTest(client);
-          }
-          _pieceTest.pieceTest(client, front);
-          break;
-        case TorrentMessage.SIGN_PORT:
-          {}
-          break;
-      }
-    });
-  }
-
-  Future onSignal(TorrentClient client, TorrentClientPeerInfo info, TorrentClientSignal signal) {
-    return new Future(() {
-      switch (signal.id) {
-        case TorrentClientSignal.ID_HANDSHAKED:
-          info.front.sendBitfield(client.targetBlock.bitfield);
-          break;
-        case TorrentClientSignal.ID_ACCEPT:
-        case TorrentClientSignal.ID_CONNECTED:
-          break;
-        case TorrentClientSignal.ID_ADD_PEERINFO:
-          _connectTest.connectTest(info, client, _maxConnect);
-          break;
-        case TorrentClientSignal.ID_SET_PIECE_A_PART:
-        case TorrentClientSignal.ID_SET_PIECE:
-          if (_pieceTest == null) {
-            _pieceTest = new PieceTest(client);
-          }
-          _pieceTest.pieceTest(client, info.front);
-          break;
-      }
-    });
+  Future onSignal(TorrentClient client, TorrentClientPeerInfo info, TorrentClientSignal signal) async {
+    switch (signal.id) {
+      case TorrentClientSignal.ID_HANDSHAKED:
+        info.front.sendBitfield(client.targetBlock.bitfield);
+        break;
+      case TorrentClientSignal.ID_ACCEPT:
+      case TorrentClientSignal.ID_CONNECTED:
+        break;
+      case TorrentClientSignal.ID_ADD_PEERINFO:
+        _connectTest.connectTest(info, client, _maxConnect);
+        break;
+      case TorrentClientSignal.ID_SET_PIECE_A_PART:
+      case TorrentClientSignal.ID_SET_PIECE:
+        if (_pieceTest == null) {
+          _pieceTest = new PieceTest(client);
+        }
+        _pieceTest.pieceTest(client, info.front);
+        break;
+    }
   }
 }
--- lib/src/client/torrentai_connect.dart
+++ lib/src/client/torrentai_connect.dart
@@ -7,39 +7,41 @@ import 'torrentclientfront.dart';
 import 'torrentclientpeerinfo.dart';
 
 class ConnectTest {
-  Future connectTest(TorrentClientPeerInfo info, TorrentClient client, int _maxConnect) {
-    return new Future(() {
+  Future connectTest(TorrentClientPeerInfo info, TorrentClient client, int _maxConnect) async {
+    if (!(info.front == null || info.front.isClose == true)) {
+      return {};
+    }
+
+    List<TorrentClientPeerInfo> connectedPeers = client.rawPeerInfos.getPeerInfo((TorrentClientPeerInfo info) {
       if (info.front == null || info.front.isClose == true) {
-        List<TorrentClientPeerInfo> connects = client.rawPeerInfos.getPeerInfo((TorrentClientPeerInfo info) {
-          if (info.front == null || info.front.isClose == true) {
-            return false;
-          } else {
-            return true;
-          }
-        });
-        if (connects.length < _maxConnect && (info.front == null || info.front.amI == false)) {
-          if ((info.front != null && client.targetBlock.haveAll() == true && info.front.bitfieldToMe.isAllOn())) {
-            return null;
-          }
-          if (info.front != null && info.front.isClose == false) {
-            return null;
-          }
-          if (false == client.isStart) {
-            return null;
-          }
-          return client.connect(info).then((TorrentClientFront f) {
-            return f.sendHandshake();
-          }).catchError((e) {
-            try {
-              if (info.front != null) {
-                info.front.close();
-              }
-            } catch (e) {
-              ;
-            }
-          });
-        }
+        return false;
+      } else {
+        return true;
       }
     });
+
+    if (connectedPeers.length < _maxConnect && (info.front == null || info.front.amI == false)) {
+      if ((info.front != null && client.targetBlock.haveAll() == true && info.front.bitfieldToMe.isAllOn())) {
+        return null;
+      }
+      if (info.front != null && info.front.isClose == false) {
+        return null;
+      }
+      if (false == client.isStart) {
+        return null;
+      }
+      try {
+        TorrentClientFront f = await client.connect(info);
+        return f.sendHandshake();
+      } catch (e) {
+        try {
+          if (info.front != null) {
+            info.front.close();
+          }
+        } catch (e) {
+          ;
+        }
+      }
+    }
   }
 }
