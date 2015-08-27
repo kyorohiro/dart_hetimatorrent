b78b037b78eaea1e75b9ec3ade417c8afbe9311b
kyorohiro <kyorohiro@gmail.com>
2015-08-27 15:57:29 +0900

    rename dht const fields

--- lib/src/dht/work/knodework_basic.dart
+++ lib/src/dht/work/knodework_basic.dart
@@ -12,46 +12,46 @@ import 'knodework_announce.dart';
 import 'knodework.dart';
 
 class KNodeWorkBasic extends KNodeWork {
-  KNodeWorkFindNode findNodeAI = new KNodeWorkFindNode();
-  KNodeWorkAnnounce announceAI = new KNodeWorkAnnounce();
+  KNodeWorkFindNode _findNodeAI = new KNodeWorkFindNode();
+  KNodeWorkAnnounce _announceAI = new KNodeWorkAnnounce();
 
   KNodeWorkBasic({bool verbose: false}) {}
 
   start(KNode node) {
-    findNodeAI.start(node);
-    announceAI.start(node);
+    _findNodeAI.start(node);
+    _announceAI.start(node);
   }
 
   stop(KNode node) {
-    findNodeAI.stop(node);
-    announceAI.stop(node);
+    _findNodeAI.stop(node);
+    _announceAI.stop(node);
   }
 
   updateP2PNetwork(KNode node) {
-    findNodeAI.updateP2PNetwork(node);
-    announceAI.updateP2PNetwork(node);
+    _findNodeAI.updateP2PNetwork(node);
+    _announceAI.updateP2PNetwork(node);
   }
 
   startSearchValue(KNode node, KId infoHash, int port, {getPeerOnly: false}) {
-    return announceAI.startSearchValue(node, infoHash, port);
+    return _announceAI.startSearchValue(node, infoHash, port);
   }
 
   researchSearchPeer(KNode node, KId infoHash) {
-    announceAI.researchSearchPeer(node, infoHash);
+    _announceAI.researchSearchPeer(node, infoHash);
   }
 
   stopSearchValue(KNode node, KId infoHash) {
-    announceAI.stopSearchValue(node, infoHash);
+    _announceAI.stopSearchValue(node, infoHash);
   }
 
   onTicket(KNode node) {
-    findNodeAI.onTicket(node);
-    announceAI.onTicket(node);
+    _findNodeAI.onTicket(node);
+    _announceAI.onTicket(node);
   }
 
   onAddNodeFromIPAndPort(KNode node, String ip, int port) {
-    findNodeAI.onAddNodeFromIPAndPort(node, ip, port);
-    announceAI.onAddNodeFromIPAndPort(node, ip, port);
+    _findNodeAI.onAddNodeFromIPAndPort(node, ip, port);
+    _announceAI.onAddNodeFromIPAndPort(node, ip, port);
   }
 
   onReceiveQuery(KNode node, HetimaReceiveUdpInfo info, KrpcMessage query) {
@@ -69,31 +69,31 @@ class KNodeWorkBasic extends KNodeWork {
       default:
         return node.sendErrorResponse(info.remoteAddress, info.remotePort, KrpcMessage.METHOD_ERROR, query.transactionId).catchError((_) {});
     }
-    findNodeAI.onReceiveQuery(node, info, query);
-    announceAI.onReceiveQuery(node, info, query);
+    _findNodeAI.onReceiveQuery(node, info, query);
+    _announceAI.onReceiveQuery(node, info, query);
   }
 
   onReceiveResponse(KNode node, HetimaReceiveUdpInfo info, KrpcMessage response) {
     if (node.isStart == false) {
       return null;
     }
-    findNodeAI.onReceiveResponse(node, info, response);
-    announceAI.onReceiveResponse(node, info, response);
+    _findNodeAI.onReceiveResponse(node, info, response);
+    _announceAI.onReceiveResponse(node, info, response);
   }
 
   onReceiveError(KNode node, HetimaReceiveUdpInfo info, KrpcMessage message) {
     if (node.isStart  == false) {
       return null;
     }
-    findNodeAI.onReceiveError(node, info, message);
-    announceAI.onReceiveError(node, info, message);
+    _findNodeAI.onReceiveError(node, info, message);
+    _announceAI.onReceiveError(node, info, message);
   }
 
   onReceiveUnknown(KNode node, HetimaReceiveUdpInfo info, KrpcMessage message) {
     if (node.isStart  == false) {
       //return null;
     }
-    findNodeAI.onReceiveUnknown(node, info, message);
-    announceAI.onReceiveUnknown(node, info, message);
+    _findNodeAI.onReceiveUnknown(node, info, message);
+    _announceAI.onReceiveUnknown(node, info, message);
   }
 }
