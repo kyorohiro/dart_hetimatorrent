941c8d43ec66b5681a95b22a97e96174a1ec2c17
kyorohiro <kyorohiro@gmail.com>
2015-08-30 09:23:03 +0900

    mod async base about messagechoke

--- lib/src/client/message/torrentmessage_choke.dart
+++ lib/src/client/message/torrentmessage_choke.dart
@@ -8,39 +8,33 @@ import 'torrentmessage.dart';
 class TMessageChoke extends TorrentMessage {
   static const int CHOKE_LENGTH = 1;
 
-  TMessageChoke() : super(TorrentMessage.SIGN_CHOKE) {
-  }
+  TMessageChoke() : super(TorrentMessage.SIGN_CHOKE) {}
 
-  static Future<TMessageChoke> decode(EasyParser parser) {
-    Completer c = new Completer();
+  static Future<TMessageChoke> decode(EasyParser parser) async {
     TMessageChoke message = new TMessageChoke();
     parser.push();
-    parser.readInt(ByteOrder.BYTEORDER_BIG_ENDIAN).then((int size) {
-      if(size != CHOKE_LENGTH) {
+    try {
+      int size = await parser.readInt(ByteOrder.BYTEORDER_BIG_ENDIAN);
+      if (size != CHOKE_LENGTH) {
         throw {};
       }
-      return parser.readByte();
-    }).then((int v) {
-      if(v != TorrentMessage.SIGN_CHOKE) {
+      int v = await parser.readByte();
+      if (v != TorrentMessage.SIGN_CHOKE) {
         throw {};
       }
       parser.pop();
-      c.complete(message);
-    }).catchError((e) {
+      return message;
+    } catch (e) {
       parser.back();
       parser.pop();
-      c.completeError(e);
-    });
-    return c.future;
+    }
   }
 
-  Future<List<int>> encode() {
-    return new Future(() {
-      ArrayBuilder builder = new ArrayBuilder();
-      builder.appendIntList(ByteOrder.parseIntByte(CHOKE_LENGTH, ByteOrder.BYTEORDER_BIG_ENDIAN));
-      builder.appendByte(id);
-      return builder.toList();
-    });
+  Future<List<int>> encode() async {
+    ArrayBuilder builder = new ArrayBuilder();
+    builder.appendIntList(ByteOrder.parseIntByte(CHOKE_LENGTH, ByteOrder.BYTEORDER_BIG_ENDIAN));
+    builder.appendByte(id);
+    return builder.toList();
   }
 
   String toString() {
