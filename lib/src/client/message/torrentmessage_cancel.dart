efb891160ebdeb2f5af5d719f50d7a7ee162f5ee
kyorohiro <kyorohiro@gmail.com>
2015-08-30 09:20:08 +0900

    mod async base about messagecancel

--- lib/src/client/message/torrentmessage_cancel.dart
+++ lib/src/client/message/torrentmessage_cancel.dart
@@ -8,69 +8,56 @@ import 'torrentmessage.dart';
 class TMessageCancel extends TorrentMessage {
   static final List<int> RESERVED = new List.from([0, 0, 0, 0, 0, 0, 0, 0], growable: false);
 
-
-  static const int CANCEL_LENGTH = 1+4*3;
+  static const int CANCEL_LENGTH = 1 + 4 * 3;
   int _mIndex = 0;
   int _mBegin = 0;
   int _mLength = 0;
 
-  int get index => _mIndex; 
+  int get index => _mIndex;
   int get begin => _mBegin;
   int get length => _mLength;
 
-  TMessageCancel._empty() : super(TorrentMessage.SIGN_CANCEL) {
-  }
+  TMessageCancel._empty() : super(TorrentMessage.SIGN_CANCEL) {}
 
   TMessageCancel(int index, int begin, int length) : super(TorrentMessage.SIGN_CANCEL) {
     this._mIndex = index;
     this._mBegin = begin;
-    this._mLength= length;
+    this._mLength = length;
   }
 
-  static Future<TMessageCancel> decode(EasyParser parser) {
-    Completer c = new Completer();
-    TMessageCancel message = new TMessageCancel._empty();
+  static Future<TMessageCancel> decode(EasyParser parser) async {
     parser.push();
-    parser.readInt(ByteOrder.BYTEORDER_BIG_ENDIAN).then((int size) {
-      if(size != CANCEL_LENGTH) {
+    try {
+      int size = await parser.readInt(ByteOrder.BYTEORDER_BIG_ENDIAN);
+      if (size != CANCEL_LENGTH) {
         throw {};
       }
-      return parser.readByte();
-    }).then((int v) {
-      if(v != TorrentMessage.SIGN_CANCEL) {
+      int v = await parser.readByte();
+      if (v != TorrentMessage.SIGN_CANCEL) {
         throw {};
       }
-      return parser.readInt(ByteOrder.BYTEORDER_BIG_ENDIAN);
-    }).then((int index) {
-      message._mIndex = index;
-      return parser.readInt(ByteOrder.BYTEORDER_BIG_ENDIAN);
-    }).then((int begin) {
-      message._mBegin = begin;
-      return parser.readInt(ByteOrder.BYTEORDER_BIG_ENDIAN);
-    }).then((int length) {
-      message._mLength = length;
+      TMessageCancel message = new TMessageCancel._empty();
+      message._mIndex = await parser.readInt(ByteOrder.BYTEORDER_BIG_ENDIAN);
+      message._mBegin = await parser.readInt(ByteOrder.BYTEORDER_BIG_ENDIAN);
+      message._mLength = await parser.readInt(ByteOrder.BYTEORDER_BIG_ENDIAN);
       parser.pop();
-      c.complete(message);
-    }).catchError((e) {
+      return message;
+    } catch (e) {
       parser.back();
       parser.pop();
-      c.completeError(e);
-    });
-    return c.future;
+    }
   }
 
-  Future<List<int>> encode() {
-    return new Future(() {
-      ArrayBuilder builder = new ArrayBuilder();
-      builder.appendIntList(ByteOrder.parseIntByte(CANCEL_LENGTH, ByteOrder.BYTEORDER_BIG_ENDIAN));
-      builder.appendByte(id);
-      builder.appendIntList(ByteOrder.parseIntByte(_mIndex, ByteOrder.BYTEORDER_BIG_ENDIAN));
-      builder.appendIntList(ByteOrder.parseIntByte(_mBegin, ByteOrder.BYTEORDER_BIG_ENDIAN));
-      builder.appendIntList(ByteOrder.parseIntByte(_mLength, ByteOrder.BYTEORDER_BIG_ENDIAN));
-      return builder.toList();
-    });
+  Future<List<int>> encode() async {
+    ArrayBuilder builder = new ArrayBuilder();
+    builder.appendIntList(ByteOrder.parseIntByte(CANCEL_LENGTH, ByteOrder.BYTEORDER_BIG_ENDIAN));
+    builder.appendByte(id);
+    builder.appendIntList(ByteOrder.parseIntByte(_mIndex, ByteOrder.BYTEORDER_BIG_ENDIAN));
+    builder.appendIntList(ByteOrder.parseIntByte(_mBegin, ByteOrder.BYTEORDER_BIG_ENDIAN));
+    builder.appendIntList(ByteOrder.parseIntByte(_mLength, ByteOrder.BYTEORDER_BIG_ENDIAN));
+    return builder.toList();
   }
-  
+
   String toString() {
     return "${TorrentMessage.toText(id)}:${_mIndex} ${_mBegin} ${_mLength}";
   }
